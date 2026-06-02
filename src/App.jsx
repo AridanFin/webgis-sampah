c651283513e8ceed576f58a2fd2de50425e11d49
AridanFin <arrr.23230009@gmail.com>
2026-06-02 12:43:53 +0700

    add login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "./pages/login";
+import Register from "./pages/register";
 import Warga from "./pages/warga";
 import Transporter from "./pages/transporter";
 import Admin from "./pages/admin";
 
 function App() {
-
   return (
     <BrowserRouter>
-
       <Routes>
-
-        <Route path="/" element={<Warga />} />
-
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/warga" element={<Warga />} />
         <Route path="/transporter" element={<Transporter />} />
-
         <Route path="/admin" element={<Admin />} />
-
       </Routes>
-
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
